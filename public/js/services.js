3ef3eb0151863447d53037f106c78b26f5d7d12d
Yuriy Remnev <yura_remnev@mail.ru>
2014-07-27 21:49:27 +0400

    Set active item in the left menu on a services page

--- public/js/services.js
+++ public/js/services.js
@@ -8,4 +8,41 @@ $(function() {
             }
         }
     });
+
+    // set active item in the left menu
+    var didScroll = false,
+        $servicesSet = $('.service');
+
+    $(window).scroll(function() {
+        didScroll = true;
+    }).scroll();
+
+    setInterval(function() {
+        if (didScroll) {
+            didScroll = false;
+
+            $servicesSet.each(function() {
+                if (isScrolledIntoView(this)) {
+                    $(this).siblings('.active').removeClass('active').end()
+                           .addClass('active');
+
+                    $('.services-list__item').has('a[href=#'+this.id+']').siblings('.active').removeClass('active').end()
+                                             .addClass('active');
+
+                    return false;
+                }
+            });
+        }
+    }, 500);
+
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top + 150;
+        var elemBottom = elemTop + $(elem).height();
+
+        return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom)
+          && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
+    }
 });
